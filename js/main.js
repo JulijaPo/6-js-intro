6f7e606dd6948f571d1dd647af60cc1b4dee6128
Your Name <you@example.com>
2021-01-14 11:25:54 +0200

    mini uzd 1 ir 2

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//
+
+// vienos eiles kom
+
+
+/*
+keletos
+eiluciu
+*/
+console.log(123);
+console.log('Labas rytas');
+
+const metai = 2021;
+const menuo = 01;
+const diena = 01;
+console.log(metai, menuo, diena);
+let vardas = 'Jonas';
+console.log('Mano vardas', vardas );
+vardas = 'Pertas';
+console.log('Mano vardas', vardas);
+vardas = 'Tomas';
+console.log('Mano vardas', vardas);
+const skaiciai = [1,2,3,7,8];
+console.log(skaiciai);
+const pirmas = skaiciai[0];
+const antras = skaiciai[1];
+const trecias = skaiciai[2];
+const ketvirtas = skaiciai[3];
+const penktas = skaiciai[4];
+const sum = pirmas+antras+trecias+ketvirtas+penktas;
+console.log(sum);
+console.log(pirmas-antras+trecias-ketvirtas+penktas);
+
+const vardai = ['Jonas','Petras', 'Tomas','Ana', 'Lukas'];
+const pirmasvardas = vardai[0];
+const antrasvardas = vardai[1];
+const treciasvardas = vardai[2];
+const ketvirtasvardas = vardai[3];
+const penktasvardas = vardai[4];
+console.log(vardai);
+const vardaiTarpas = pirmasvardas+' '+antrasvardas+' '+treciasvardas+' '+ketvirtasvardas+' '+penktasvardas;
+console.log(vardaiTarpas);
+const vardaiKablelisIrTarpas = penktasvardas+', '+ketvirtasvardas+', '+treciasvardas+', '+antrasvardas+', '+pirmasvardas;
+console.log(vardaiKablelisIrTarpas);
